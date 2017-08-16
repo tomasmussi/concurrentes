d2a40970b666778fab372a17e5d8257a44403b91
tomas <tomasmussi@gmail.com>
2017-08-16 15:37:30 -0300

    Agrego una base de prueba con clases y un makefile para compilar en cpp

---
+++ prueba/Prueba.cpp
@@ -0,0 +1,20 @@
+#include "Prueba.h"
+
+Prueba::Prueba() {
+}
+
+bool Prueba::superficieEdificada() {
+	return false;
+}
+
+bool Prueba::superficieArbolada() {
+	return false;
+}
+
+const char Prueba::caracter() {
+	return '-';
+}
+
+int Prueba::nivel() {
+	return 1;
+}
---
+++ prueba/Prueba.h
@@ -0,0 +1,13 @@
+#ifndef PRUEBA_H
+#define PRUEBA_H
+
+class Prueba {
+public:
+	explicit Prueba();
+	bool superficieEdificada();
+	bool superficieArbolada();
+	const char caracter();
+	int nivel();
+};
+
+#endif /* PRUEBA_H */
