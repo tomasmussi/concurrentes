72f1cebe14d446dd5a106ece4743c859f191af05
Pipi <p.ciruzzi@hotmail.com>
2017-10-07 20:08:45 -0300

    Code clean up

--- tp1/ipc/MemoriaCompartida.h
+++ tp1/ipc/MemoriaCompartida.h
@@ -1,7 +1,6 @@
 #ifndef TP1_MEMORIACOMPARTIDA_H
 #define TP1_MEMORIACOMPARTIDA_H
 
-
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -13,90 +12,85 @@
 
 #include <sstream>
 
-#define SHM_OK           0
-#define ERROR_FTOK      -1
-#define ERROR_SHMGET    -2
-#define ERROR_SHMAT     -3
-
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string>
-
-
 template <class T> class MemoriaCompartida {
 
 private:
     int shmId;
-    T*  ptrDatos;
+    T* ptrDatos;
 
-    int cantidadProcesosAdosados () const;
+    int cantidadProcesosAdosados() const;
 
 public:
-    MemoriaCompartida ();
-    ~MemoriaCompartida ();
-    int crear ( const std::string& archivo,const char letra );
-    void liberar ();
-    void escribir ( const T& dato );
-    T leer () const;
-
+    MemoriaCompartida();
+    ~MemoriaCompartida();
+    int crear(const std::string& archivo, const char letra);
+    void liberar();
+    void escribir(const T& dato);
+    T leer() const;
 };
 
-template <class T> MemoriaCompartida<T> :: MemoriaCompartida() : shmId(0), ptrDatos(NULL) {
+template <class T> MemoriaCompartida<T>::MemoriaCompartida() : shmId(0), ptrDatos(NULL) {
 }
 
-template <class T> MemoriaCompartida<T> :: ~MemoriaCompartida() {
+template <class T> MemoriaCompartida<T>::~MemoriaCompartida() {
 }
 
-template <class T> int MemoriaCompartida<T> :: crear ( const std::string& archivo,const char letra ) {
+template <class T> int MemoriaCompartida<T>::crear(const std::string& archivo, const char letra) {
+    key_t clave = ftok(archivo.c_str(), letra);
 
-    // generacion de la clave
-    key_t clave = ftok ( archivo.c_str(),letra );
-    if ( clave == -1 )
-        return ERROR_FTOK;
-    else {
-        // creacion de la memoria compartida
-        this->shmId = shmget ( clave,sizeof(T),0644|IPC_CREAT );
+    if (clave > 0) {
+        this->shmId = shmget(clave, sizeof(T), 0644|IPC_CREAT);
 
-        if ( this->shmId == -1 )
-            return ERROR_SHMGET;
-        else {
-            // attach del bloque de memoria al espacio de direcciones del proceso
-            void* ptrTemporal = shmat ( this->shmId,NULL,0 );
-
-            if ( ptrTemporal == (void *) -1 ) {
-                return ERROR_SHMAT;
+        if (this->shmId > 0) {
+            void* tmpPtr = shmat(this->shmId, NULL, 0);
+            if (tmpPtr != (void*) -1) {
+                this->ptrDatos = static_cast<T*>(tmpPtr);
             } else {
-                this->ptrDatos = static_cast<T*> (ptrTemporal);
-                return SHM_OK;
+                Logger::log("SHM", Logger::WARNING, "Error al crear 1", Logger::get_date());
+                std::string mensaje = std::string("Error en shmat(): ") + std::string(strerror(errno));
+                throw mensaje;
             }
+        } else {
+            Logger::log("SHM", Logger::WARNING, "Error al crear 2", Logger::get_date());
+            std::string mensaje = std::string("Error en shmget(): ") + std::string(strerror(errno));
+            throw mensaje;
         }
+    } else {
+        Logger::log("SHM", Logger::WARNING, "Error al crear 3", Logger::get_date());
+        std::string mensaje = std::string("Error en ftok(): ") + std::string(strerror(errno));
+        throw mensaje;
     }
 }
 
 
 template <class T> void MemoriaCompartida<T> :: liberar () {
-    // detach del bloque de memoria
-    shmdt ( static_cast<void*> (this->ptrDatos) );
+    int errorDt = shmdt((void *)this->ptrDatos);
 
-    int procAdosados = this->cantidadProcesosAdosados ();
-
-    if ( procAdosados == 0 ) {
-        shmctl ( this->shmId,IPC_RMID,NULL );
+    if (errorDt != -1) {
+        int procAdosados = this->cantidadProcesosAdosados();
+        if (procAdosados == 0) {
+            shmctl(this->shmId, IPC_RMID, NULL);
+        }
+    } else {
+        std::stringstream s;
+        s << "[" << getpid() << "] Error al liberar: " << std::string(strerror(errno));
+        Logger::log("SHM", Logger::WARNING, s.str(), Logger::get_date());
+        std::string mensaje = std::string("Error en shmdt(): ") + std::string(strerror(errno));
+        throw mensaje;
     }
 }
 
-template <class T> void MemoriaCompartida<T> :: escribir ( const T& dato ) {
-    * (this->ptrDatos) = dato;
+template <class T> void MemoriaCompartida<T>::escribir(const T& dato) {
+    *(this->ptrDatos) = dato;
 }
 
-template <class T> T MemoriaCompartida<T> :: leer () const {
-    return ( *(this->ptrDatos) );
+template <class T> T MemoriaCompartida<T>::leer() const {
+    return (*(this->ptrDatos));
 }
 
-template <class T> int MemoriaCompartida<T> :: cantidadProcesosAdosados () const {
+template <class T> int MemoriaCompartida<T>::cantidadProcesosAdosados() const {
     shmid_ds estado;
-    shmctl ( this->shmId,IPC_STAT,&estado );
+    shmctl(this->shmId, IPC_STAT, &estado);
     return estado.shm_nattch;
 }
 
