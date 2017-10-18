7fea2754e9719423c1d01d85746791f7aadb59f0
tomas <tomasmussi@gmail.com>
2017-10-18 17:01:45 -0300

    Pruebas con semaforo funcionando, O_CREAT POR IPC_CREATgit status !

--- prueba/06-semaforos/main.cpp
+++ prueba/06-semaforos/main.cpp
@@ -16,35 +16,50 @@
 #include <sys/sem.h>
 
 
-#define MAX 100
+#define N 5
 
 int main(int argc, char* argv[]) {
 
 	key_t key = ftok("main.cpp", 'a');
-	int semId = semget(key, 1, O_CREAT | 0644);
+	int semId = semget(key, 1, IPC_CREAT | 0644);
 
 	union semun {
-       int              val;    /* Value for SETVAL */
-       struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-       unsigned short  *array;  /* Array for GETALL, SETALL */
-       struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                   (Linux-specific) */
+       int              val;    // Value for SETVAL
+       struct semid_ds *buf;    // Buffer for IPC_STAT, IPC_SET
+       unsigned short  *array;  // Array for GETALL, SETALL
+       struct seminfo  *__buf;  // Buffer for IPC_INFO
 	};
 	semun asd;
-	asd.val = 3;
+	asd.val = 2; // Arranca en 1 el semaforo
 
-	semctl(semId, 0, IPC_SET, asd);
+	semctl(semId, 0, SETVAL, asd);
 
-	pid_t pid = fork();
+	for (int i = 0; i < N; i++) {
+		pid_t pid = fork();
 
-	if (pid == 0) {
+		if (pid == 0) {
+			struct sembuf buf;
+			memset(&buf, 0, sizeof(struct sembuf));
+			buf.sem_num = 0;
+			buf.sem_op = -1;
+			//buf.sem_flg = SEM_UNDO;
 
+			semop(semId, &buf, 1); // 1 solo semaforo
+			std::cout << "[" << getpid() << "] semaforo tomado" << std::endl;
+			sleep(4);
 
-		_exit(0);
-	} else {
+			buf.sem_op = 1;
+			std::cout << "[" << getpid() << "] por liberar semaforo" << std::endl;
+			semop(semId, &buf, 1);
 
-		wait(NULL);
-		_exit(0);
+			_exit(0);
+		}
 	}
+
+	for (int i = 0; i < N; i++) {
+		wait(NULL);
+	}
+
+	semctl(semId, 0,IPC_RMID);
 	return 0;
 }
