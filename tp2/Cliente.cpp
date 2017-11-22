5c0288ef6ca4857d24593b7dc103d1a6a5687b6c
Pipi <p.ciruzzi@hotmail.com>
2017-11-22 01:29:07 -0300

    Agrego protocolo basico entre cliente, servidor y servicios

--- tp2/Cliente.cpp
+++ tp2/Cliente.cpp
@@ -1,17 +1,30 @@
 #include <cstring>
 #include <iostream>
 #include "Cliente.h"
+#include "Constantes.h"
 
 Cliente::Cliente(int tipoCliente, char* request) : cola(MSG_ARCHIVO, CHAR_CLIENTE_SERVIDOR),
                                                    _tipoCliente(tipoCliente), _request(request) {}
 
 void Cliente::ejecutar() {
     mensaje m;
-    m.mtype = _tipoCliente;
-    m.id = _tipoCliente + 2;
-    strcpy(m.texto, _request);
+    m.mtype = NUEVA_CONEXION;
     cola.escribir(m);
-    mensaje rta;
-    cola.leer(m.id, &rta);
-    std::cout << rta.texto << std::endl;
+    int resultado = cola.leer(RESPUESTA_NUEVA_CONEXION, &m);
+    if (resultado != -1) {
+        // El request va a ser a donde me indica el handShake
+        int idCliente = m.id;
+        m.mtype = idCliente;
+        m.id = _tipoCliente;
+        strcpy(m.texto, _request);
+        cola.escribir(m);
+
+        // Espero la respuesta en idCliente + 1
+        mensaje rta;
+        resultado = cola.leer(idCliente + 1, &rta);
+        if (resultado != -1) {
+            std::cout << "Cliente " << idCliente << " pidio por " << _tipoCliente << " " << _request <<
+                " y recibio '" << rta.texto << "'" << std::endl;
+        }
+    }
 }
