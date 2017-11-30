d461a8c6219c6f006eb3f595108a5d71f9339c40
Pipi <p.ciruzzi@hotmail.com>
2017-11-30 01:28:40 -0300

    try catch en Cliente

--- tp2/Cliente.cpp
+++ tp2/Cliente.cpp
@@ -7,25 +7,29 @@ Cliente::Cliente(int tipoCliente, char* request) : cola(MSG_ARCHIVO, CHAR_CLIENT
                                                    _tipoCliente(tipoCliente), _request(request) {}
 
 void Cliente::ejecutar() {
-    mensajeCS m;
-    m.mtype = NUEVA_CONEXION;
-    cola.escribir(m);
-    int resultado = cola.leer(RESPUESTA_NUEVA_CONEXION, &m);
-    if (resultado != -1) {
-        // El request va a ser a donde me indica el handShake
-        int idCliente = m.id;
-        m.mtype = idCliente;
-        m.tipo = _tipoCliente;
-        m.admin = false;
-        strcpy(m.texto, _request);
+    try {
+        mensajeCS m;
+        m.mtype = NUEVA_CONEXION;
         cola.escribir(m);
-
-        // Espero la respuesta en idCliente + 1
-        mensajeCS rta;
-        resultado = cola.leer(idCliente + 1, &rta);
+        int resultado = cola.leer(RESPUESTA_NUEVA_CONEXION, &m);
         if (resultado != -1) {
-            std::cout << "Cliente " << idCliente << " pidio por " << _tipoCliente << " " << _request <<
-                " y recibio '" << rta.texto << "'" << std::endl;
+            // El request va a ser a donde me indica el handShake
+            int idCliente = m.id;
+            m.mtype = idCliente;
+            m.tipo = _tipoCliente;
+            m.admin = false;
+            strcpy(m.texto, _request);
+            cola.escribir(m);
+
+            // Espero la respuesta en idCliente + 1
+            mensajeCS rta;
+            resultado = cola.leer(idCliente + 1, &rta);
+            if (resultado != -1) {
+                std::cout << "Cliente " << idCliente << " pidio por " << _tipoCliente << " " << _request <<
+                    " y recibio '" << rta.texto << "'" << std::endl;
+            }
         }
+    } catch (std::string e) {
+        std::cout << "No se ha podido comunicar con el servidor. Cerrando conexión..." << std::endl;
     }
 }
